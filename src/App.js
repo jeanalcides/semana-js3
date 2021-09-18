afaaad7c8750dc7954fd6a91ad621c267b487998
jeanalcides <jean.ap44@gmail.com>
2021-09-17 21:53:57 -0300

    Set completed word and get a new one

--- src/App.js
+++ src/App.js
@@ -36,11 +36,26 @@ const App = () => {
 
     const [typedKeys, setTypedKeys] = useState([]);
     const [validKeys, setValidKeys] = useState([]);
+    const [completedWords, setCompletedWords] = useState([]);
     const [word, setWord] = useState('');
 
     useEffect(() => {
         setWord(getWord());
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        const wordFromValidKeys = validKeys.join('').toLowerCase();
+        if(word && word === wordFromValidKeys){
+            let newWord = null;
+            do{
+                newWord = getWord();
+            }while(completedWords.includes(newWord));
+
+            setWord(newWord);
+            setValidKeys([]);
+            setCompletedWords((prev) => [...prev, word]);
+        }
+    }, [word, validKeys, completedWords]);
 
     const handleKeyDown = (e) => {
         e.preventDefault();
@@ -61,9 +76,9 @@ const App = () => {
             });
         }
 
-        console.log('key', key);
-
     }
+
+
 
     return (
         <div className="container" tabIndex="0" onKeyDown={handleKeyDown}>
@@ -73,9 +88,7 @@ const App = () => {
             <div className="typed-keys">{typedKeys ? typedKeys.join(' ') : null}</div>
             <div className="completed-words">
                 <ol>
-                    <li>cidade</li>
-                    <li>carro</li>
-                    <li>profisional</li>
+                    {completedWords.map((word) => (<li key={word}>{word}</li>))}
                 </ol>
             </div>
         </div>
